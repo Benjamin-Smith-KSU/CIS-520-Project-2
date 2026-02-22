ddfe27f0974e20f7564bd547149acfb80ed1810b
Ben Smith <benjaminsmith@ksu.edu>
2026-02-21 20:54:00 -0600

    Added code to analysis section, this code is untested as I am waiting for FSFC

--- hw2/src/analysis.c
+++ hw2/src/analysis.c
@@ -12,13 +12,53 @@
 
 // Add and comment your analysis code in this function.
 // THIS IS NOT FINISHED.
-int main(int argc, char **argv) {
-  if (argc < 3) {
+int main(int argc, char **argv) 
+{
+  //check if the right number of arguments were pashed
+  if (argc < 3) 
+  {
     printf("%s <pcb file> <schedule algorithm> [quantum]\n", argv[0]);
     return EXIT_FAILURE;
   }
 
-  abort(); // REPLACE ME with implementation.
+  //read in the file name
+  char *filename = argv[1];
+  //read in the algorithm 
+  char *algorithm = argv[2];
 
+
+  //Load ready queue
+  dyn_array_t *ready_queue = load_process_control_blocks(filename);
+  //check if memory loaded
+  if(ready_queue == NULL)
+  {
+    fprintf(stderr, "Error: Could not load PCB file %s\n", filename);
+    return EXIT_FAILURE;
+  }
+
+  //create output stuct
+  ScheduleResult_t result = {0, 0, 0};
+  bool success = false;
+
+  //identify algorithm and run it.
+  if (strncmp(algorithm, FCFS, 5) == 0) 
+  {
+    success = first_come_first_serve(ready_queue, &result);
+  } 
+  else 
+  {
+    fprintf(stderr, "Error: Algorithm '%s' is not supported in Checkpoint 2.\n", algorithm);
+  }
+
+  //print the results
+  if (success) 
+  {
+      printf("Average Waiting Time:    %.2f\n", result.average_waiting_time);
+      printf("Average Turnaround Time: %.2f\n", result.average_turnaround_time);
+      printf("Total Clock Time:        %lu\n", result.total_run_time);
+  }
+
+  //clean up alocated memory
+  dyn_array_destroy(ready_queue);
   return EXIT_SUCCESS;
 }
