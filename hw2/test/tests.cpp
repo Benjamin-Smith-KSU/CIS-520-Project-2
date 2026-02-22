1c0967ac6e24949ee111b112fa0c2d3223737737
jseimKSU <jseim@ksu.edu>
2026-02-21 21:01:02 -0600

    added more tests

--- hw2/test/tests.cpp
+++ hw2/test/tests.cpp
@@ -95,19 +95,89 @@ TEST(fcfs, GoodParams){
 }
 
 TEST(fcfs, SinglePCB){
+        ScheduleResult_t * result = (ScheduleResult_t *)malloc(sizeof(ScheduleResult_t));
+        dyn_array_t * queue = dyn_array_create(NUM_PCB, sizeof(ProcessControlBlock_t), NULL);
 
+        ProcessControlBlock_t block;
+        block.remaining_burst_time = 2;
+        block.priority = 0;
+        block.arrival = 1;
+        block.started = false;
+
+        dyn_array_push_front(queue, &block);
+                
+        bool res = first_come_first_serve(queue, result);
+        ASSERT_EQ(true, res);
+        ASSERT_EQ(0.0f, result->average_waiting_time);// test avg waiting time
+        ASSERT_EQ(2.0f, result->average_turnaround_time);// test avg turnaround time
+        ASSERT_EQ(3UL, result ->total_run_time);// test total run time
+        free(result);
+        dyn_array_destroy(queue);
 }
 
 TEST(fcfs, MultipleProcessesSameArrival){
+        ScheduleResult_t * result = (ScheduleResult_t *)malloc(sizeof(ScheduleResult_t));
+        dyn_array_t * queue = dyn_array_create(NUM_PCB, sizeof(ProcessControlBlock_t), NULL);
 
+        // given sorted
+        for(size_t i = 0; i < 2; i++){
+                ProcessControlBlock_t block;
+                block.remaining_burst_time = 2;
+                block.priority = 0;
+                block.arrival = 1;
+                block.started = false;
+
+                dyn_array_push_back(queue, &block);
+        }
+        bool res = first_come_first_serve(queue, result);
+        ASSERT_EQ(true, res);
+        ASSERT_EQ(1.0f, result->average_waiting_time);// test avg waiting time
+        ASSERT_EQ(3.0f, result->average_turnaround_time);// test avg turnaround time
+        ASSERT_EQ(5UL, result ->total_run_time);// test total run time
+        free(result);
+        dyn_array_destroy(queue);
 }
 
 TEST(fcfs, CPUIdle){
+        ScheduleResult_t * result = (ScheduleResult_t *)malloc(sizeof(ScheduleResult_t));
+        dyn_array_t * queue = dyn_array_create(NUM_PCB, sizeof(ProcessControlBlock_t), NULL);
 
+        ProcessControlBlock_t block;
+        block.remaining_burst_time = 2;
+        block.priority = 0;
+        block.arrival = 10;
+        block.started = false;
+
+        dyn_array_push_front(queue, &block);
+                
+        bool res = first_come_first_serve(queue, result);
+        ASSERT_EQ(true, res);
+        ASSERT_EQ(0.0f, result->average_waiting_time);// test avg waiting time
+        ASSERT_EQ(2.0f, result->average_turnaround_time);// test avg turnaround time
+        ASSERT_EQ(12UL, result ->total_run_time);// test total run time
+        free(result);
+        dyn_array_destroy(queue);
 }
 
 TEST(fcfs, ZeroBurstTime){
-        
+        ScheduleResult_t * result = (ScheduleResult_t *)malloc(sizeof(ScheduleResult_t));
+        dyn_array_t * queue = dyn_array_create(NUM_PCB, sizeof(ProcessControlBlock_t), NULL);
+
+        ProcessControlBlock_t block;
+        block.remaining_burst_time = 0;
+        block.priority = 0;
+        block.arrival = 1;
+        block.started = false;
+
+        dyn_array_push_front(queue, &block);
+                
+        bool res = first_come_first_serve(queue, result);
+        ASSERT_EQ(true, res);
+        ASSERT_EQ(0.0f, result->average_waiting_time);// test avg waiting time
+        ASSERT_EQ(0.0f, result->average_turnaround_time);// test avg turnaround time
+        ASSERT_EQ(1UL, result ->total_run_time);// test total run time
+        free(result);
+        dyn_array_destroy(queue);
 }
 
 
@@ -143,30 +213,30 @@ int main(int argc, char **argv) {
 TEST(load_pcbs, AssignmentProvidedPCBS) {
   dyn_array_t *pcbs = load_process_control_blocks("../pcb.bin");
 
-  ASSERT_EQ(4, dyn_array_size(pcbs));
+  ASSERT_EQ(4UL, dyn_array_size(pcbs));
 
   ProcessControlBlock_t pcb_0 = *(ProcessControlBlock_t *)dyn_array_at(pcbs, 0);
-  ASSERT_EQ(15, pcb_0.remaining_burst_time);
-  ASSERT_EQ(0, pcb_0.priority);
-  ASSERT_EQ(0, pcb_0.arrival);
+  ASSERT_EQ(15UL, pcb_0.remaining_burst_time);
+  ASSERT_EQ(0UL, pcb_0.priority);
+  ASSERT_EQ(0UL, pcb_0.arrival);
   ASSERT_EQ(0, pcb_0.started);
 
   ProcessControlBlock_t pcb_1 = *(ProcessControlBlock_t *)dyn_array_at(pcbs, 1);
-  ASSERT_EQ(10, pcb_1.remaining_burst_time);
-  ASSERT_EQ(0, pcb_1.priority);
-  ASSERT_EQ(1, pcb_1.arrival);
+  ASSERT_EQ(10UL, pcb_1.remaining_burst_time);
+  ASSERT_EQ(0UL, pcb_1.priority);
+  ASSERT_EQ(1UL, pcb_1.arrival);
   ASSERT_EQ(0, pcb_1.started);
 
   ProcessControlBlock_t pcb_2 = *(ProcessControlBlock_t *)dyn_array_at(pcbs, 2);
-  ASSERT_EQ(5, pcb_2.remaining_burst_time);
-  ASSERT_EQ(0, pcb_2.priority);
-  ASSERT_EQ(2, pcb_2.arrival);
+  ASSERT_EQ(5UL, pcb_2.remaining_burst_time);
+  ASSERT_EQ(0UL, pcb_2.priority);
+  ASSERT_EQ(2UL, pcb_2.arrival);
   ASSERT_EQ(0, pcb_2.started);
 
   ProcessControlBlock_t pcb_3 = *(ProcessControlBlock_t *)dyn_array_at(pcbs, 3);
-  ASSERT_EQ(20, pcb_3.remaining_burst_time);
-  ASSERT_EQ(0, pcb_3.priority);
-  ASSERT_EQ(3, pcb_3.arrival);
+  ASSERT_EQ(20UL, pcb_3.remaining_burst_time);
+  ASSERT_EQ(0UL, pcb_3.priority);
+  ASSERT_EQ(3UL, pcb_3.arrival);
   ASSERT_EQ(0, pcb_3.started);
 }
 
