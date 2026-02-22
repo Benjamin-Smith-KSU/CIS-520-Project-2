74a34e5b146bb468c9fbf1a831890ad8544cac77
jseimKSU <jseim@ksu.edu>
2026-02-21 19:10:36 -0600

    added good params test

--- hw2/test/tests.cpp
+++ hw2/test/tests.cpp
@@ -18,11 +18,11 @@ TEST(first_come_first_serve, BadParams){
 
         dyn_array_t * queue = dyn_array_create(0, sizeof(ProcessControlBlock_t), NULL);
         ASSERT_EQ(false, first_come_first_serve(queue, result)); // capacity == 0
-        free(queue);
+        dyn_array_destroy(queue);
 
         queue = dyn_array_create(NUM_PCB, 0, NULL);
         ASSERT_EQ(false, first_come_first_serve(queue, result)); // data_size == 0
-        free(queue);
+        dyn_array_destroy(queue);
 
         queue = dyn_array_create(NUM_PCB, sizeof(ProcessControlBlock_t), NULL);
         ASSERT_EQ(false, first_come_first_serve(NULL, result));
@@ -30,11 +30,63 @@ TEST(first_come_first_serve, BadParams){
 
         ASSERT_EQ(false, first_come_first_serve(queue, result)); // count == 0
 
-        free(queue);
+        dyn_array_destroy(queue);
         free(result);
 }
 
+
 TEST(first_come_first_serve, GoodParams){
+        ScheduleResult_t * result = malloc(sizeof(ScheduleResult_t));
+        dyn_array_t * queue = dyn_array_create(NUM_PCB, sizeof(ProcessControlBlock_t), NULL);
+
+        // given sorted
+        for(size_t i = 0; i < 10; i++){
+                ProcessControlBlock_t block;
+                block.remaining_burst_time = (i < 1) ? 1 : i * 2;
+                block.priority = 0;
+                block.arrival = i;
+                block.started = false;
+
+                dyn_array_push_back(queue, &block);
+        }
+        bool res = first_come_first_serve(queue, result);
+        ASSERT_EQ(true, res);
+        ASSERT_EQ(20, result->average_waiting_time);// test avg waiting time
+        ASSERT_EQ(29, result->average_turnaround_time);// test avg turnaround time
+        ASSERT_EQ(91, result ->total_run_time);// test total run time
+        free(result);
+        dyn_array_destroy(queue);
+
+        result = malloc(sizeof(ScheduleResult_t));
+        queue = dyn_array_create(NUM_PCB, sizeof(ProcessControlBlock_t), NULL);
+
+        // given unsorted
+        for(size_t i = 0; i < 10; i+=2){ // even indicies
+                ProcessControlBlock_t block;
+                block.remaining_burst_time = (i < 1) ? 1 : i * 2;
+                block.priority = 0;
+                block.arrival = i;
+                block.started = false;
+
+                dyn_array_push_front(queue, &block);
+        }
+        for(size_t i = 1; i < 10; i+=2){ // odd indicies
+                ProcessControlBlock_t block;
+                block.remaining_burst_time = i * 2;
+                block.priority = 0;
+                block.arrival = i;
+                block.started = false;
+
+                dyn_array_push_front(queue, &block);
+        }
+        bool res = first_come_first_serve(queue, result);
+
+        ASSERT_EQ(true, res);
+        ASSERT_EQ(20, result->average_waiting_time);// test avg waiting time
+        ASSERT_EQ(29, result->average_turnaround_time);// test avg turnaround time
+        ASSERT_EQ(91, result ->total_run_time);// test total run time
+        free(result);
+        dyn_array_destroy(queue);
 
 }
 
