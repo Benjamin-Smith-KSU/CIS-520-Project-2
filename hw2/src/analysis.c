56554bd16ccf3b02a70c6fc62116a270195dc023
Ben Smith <benjaminsmith@ksu.edu>
2026-02-28 14:23:37 -0600

    Completed Analysis.c main to handle all algorithms

--- hw2/src/analysis.c
+++ hw2/src/analysis.c
@@ -11,8 +11,8 @@
 #define SRT "SRT"
 
 /// @brief This program test the average wait, turnaround, and clock time
-/// @param argc 
-/// @param argv 
+/// @param argc Number of arguments
+/// @param argv The arguments
 /// @return 
 int main(int argc, char **argv) 
 {
@@ -27,29 +27,61 @@ int main(int argc, char **argv)
   char *filename = argv[1];
   //read in the algorithm 
   char *algorithm = argv[2];
-  //quantum not needed for only FCFS
+  //optional quantum
+  size_t quantum = 0;
+
+  //check if we have an optional quantum parm
+  if (argc >= 4) 
+  {
+    //try to read quantum in as size_t
+    if (sscanf(argv[3], "%zu", &quantum) != 1) 
+    {
+      //quantum read in failed
+      fprintf(stderr, "Error: Invalid quantum value '%s'. Must be a positive integer.\n", argv[3]);
+      return EXIT_FAILURE;
+    }
+  }
 
   //Load ready queue
   dyn_array_t *ready_queue = load_process_control_blocks(filename);
-  //check if memory loaded
+
+  //check if PCB loaded
   if(ready_queue == NULL)
   {
+    //could not load pcb
     fprintf(stderr, "Error: Could not load PCB file %s\n", filename);
     return EXIT_FAILURE;
   }
 
   //create output stuct
   ScheduleResult_t result = {0, 0, 0};
+  //create successes variable and set to false
   bool success = false;
 
   //identify algorithm and run it.
-  if (strncmp(algorithm, FCFS, 5) == 0) 
+  if (strncmp(algorithm, FCFS, 5) == 0) //First Come First Serve
   {
     success = first_come_first_serve(ready_queue, &result);
   } 
+  else if(strncmp(algorithm, SJF, 5) == 0)  //Shortest Job First
+  {
+    success = shortest_job_first(ready_queue, &result);
+  }
+  else if(strncmp(algorithm, P, 5) == 0)  //Priority
+  {
+    success = priority(ready_queue, &result);
+  }
+  else if(strncmp(algorithm, RR, 5) == 0)  //Round Robin
+  {
+    success = round_robin(ready_queue, &result, quantum);
+  }
+  else if(strncmp(algorithm, SRT, 5) == 0)  //Shortest Remaining Time first
+  {
+    success = shortest_remaining_time_first(ready_queue, &result);
+  }
   else 
   {
-    fprintf(stderr, "Error: Algorithm '%s' is not supported in Checkpoint 2.\n", algorithm);
+    fprintf(stderr, "Error: Algorithm '%s' is not supported.\n", algorithm);
   }
 
   //print the results
